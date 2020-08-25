9155493654da88aaedb64707562fe9ded683dd9e
unknown <deathbarrow@gmail.com>
2020-08-24 17:46:06 -0700

    New files

---
+++ source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <ctime>
+#include <Windows.h>
+
+int main()
+{
+int var;
+std::cin>>var;
+Sleep(2000);
+printf("Hello World");
+Sleep(2000);
+return 0;
+}
